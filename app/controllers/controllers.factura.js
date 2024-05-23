2e36f8629a9d185d3af108d279c31ec72a9c6ca1
marithzacasta <marithzacastano9.5@gmail.com>
2024-05-23 08:49:05 -0500

    Hasta aqui ya cree las rutas y confime se lo del config funcionaba como las rutas y estan bien

---
+++ app/controllers/controllers.factura.js
@@ -0,0 +1,21 @@
+// import pool from "../config/db.mysql.js";
+
+export const mostrarFactura = (req, res) =>{
+    // res.json({"respuesta" : "funciona las rutas"});
+}
+
+export const ListarFactura = (req, res) =>{
+    
+}
+
+export const crearFactura = (req, res) =>{
+    
+}
+
+export const modificarFactura = (req, res) =>{
+    
+}
+
+export const eliminarFactura = (req, res) =>{
+    
+}
